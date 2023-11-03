e04425ad5930c036d495003b4d681f8eb5389f4c
DARIA S <DARIA@EXAMPLE.COM>
2023-11-03 23:56:42 +0300

    1

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const http = require('http');
+const getUsers = require('./modules/users');
 
 const server = http.createServer((req, res) => {
+    if(req.url === '/users') {
+        res.statusCode = 200;
+        res.statusMessage = "OK";
+        res.setHeader = "Content-Type: application/json";
+        res.write(getUsers());
+        res.end();
+    }
+
     res.statusCode = 200;
     res.statusMessage = "OK";
     res.setHeader = "Content-Type: text/plain";
